875bd408ccb8932d1472923066edeb3ea47638e5
delaquash <olaidemmanuel1@gmail.com>
2019-10-09 14:38:44 +0100

    Worked on the functionalities using vanillaJS

---
+++ dist/js/main.js
@@ -0,0 +1,23 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems =  document.querySelectorAll('.nav-item');
+
+
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu () {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+    } else {
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set Menu State
+        showMenu = false;
+    }
+}
